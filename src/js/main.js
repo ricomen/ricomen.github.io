577bd498b536bf54161c310594d7514c502a3006
Khamitov Timur <ricomen@mail.ru>
2017-03-07 13:47:43 +0500

    fix adptive and js

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,47 @@
 (function() {
-var myBox = document.querySelector(".index-content");
-var myBoxHeight = myBox.offsetHeight;
-var headerHeight = document.querySelector(".page-header").offsetHeight;  
+  var mainNav = document.querySelector(".main-nav");
+  var mainNavToggle = document.querySelector(".main-nav__toggle");
 
-function boxCenter() {  
-  if (((document.documentElement.clientHeight - myBox.offsetHeight)/2) > headerHeight) {
-  myBox.style.marginTop = ((document.documentElement.clientHeight - myBox.offsetHeight)/2) - headerHeight + "px";
+  mainNavToggle.addEventListener("click", function(e) {
+    e.preventDefault();
+    mainNav.classList.toggle("main-nav--closed");
+    mainNavToggle.classList.toggle("main-nav__toggle--close");
+  });
+
+  mainNav.classList.remove('main-nav--nojs');
+
+  if(document.querySelector(".index-content")) {
+    var myBox = document.querySelector(".index-content");
+    var myBoxHeight = myBox.offsetHeight;
+    var headerHeight = document.querySelector(".page-header").offsetHeight; 
+
+    function boxCenter() {  
+    if (((document.documentElement.clientHeight - myBox.offsetHeight)/2) > headerHeight) {
+    myBox.style.marginTop = ((document.documentElement.clientHeight - myBox.offsetHeight)/2) - headerHeight + "px";
+    }
   }
-}
-boxCenter();
-window.onresize = boxCenter;
+    boxCenter();
+    window.onresize = boxCenter;
+  }  
+
+  function supportCSS(prop) {
+    var yes = false;
+    if('Moz'+prop in document.body.style) {
+        yes = true;
+    }
+    if('webkit'+prop in document.body.style) {
+        yes = true;
+    }
+    if('ms'+prop in document.body.style) {
+        yes = true;
+    }
+    if(prop in document.body.style) {
+        yes = true;
+    }
+    return yes; // возращаем ответ
+  }
+
+  if(!supportCSS("transform")) {
+    document.body.classList.add("no-css3");
+  }
 })();
